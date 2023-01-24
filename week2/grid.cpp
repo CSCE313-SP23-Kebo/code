068feb84a6d0af7f1dc3b3b4d65e789654039da8
David Kebo <davidkebo@tamu.edu>
2023-01-24 19:21:10 +0000

    Added comments

--- week2/grid.cpp
+++ week2/grid.cpp
@@ -1,6 +1,6 @@
 /*
 C++ Review
-2D Dynamic Array Example with Built-in Data Type
+2D Dynamic Array Example with Built-in Data Type (Integer)
 */
 
 
@@ -13,24 +13,30 @@ int main() {
   cout << "Enter width and height: ";
   cin >> width >> height;
 
+  // Create array (Pointer to pointer)
   int** grid = new int*[height];
+
+ // Allocate memory for rows
   for (size_t y = 0; y < height; ++y) {
     grid[y] = new int[width];
   }
 
+// Populate with numbers
   for (size_t y = 0; y < height; ++y) {
     for (size_t x = 0; x < width; ++x) {
       grid[y][x] = (x + 1) + (width * y);
     }
   }
 
+// Print content of data structure
   for (size_t y = 0; y < height; ++y) {
     for (size_t x = 0; x < width; ++x) {
       cout << grid[y][x] << " ";
     }
     cout << endl;
   }
-
+  
+// Deallocate 
   for (size_t y = 0; y < height; ++y) {
     delete[] grid[y];
   }
