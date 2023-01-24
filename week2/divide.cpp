1694ab826d9e8e30a0e3417c5f22dd00287a4311
David Kebo <davidkebo@tamu.edu>
2023-01-24 18:29:52 +0000

    C++ review

---
+++ week2/divide.cpp
@@ -0,0 +1,26 @@
+/*
+Example: Divide by zero fault, not handled.
+*/
+
+#include <iostream>
+using namespace std;
+
+float divide(float numerator, float denominator)
+{
+	return (numerator / denominator);
+}
+
+int main()
+{
+	float numerator;
+	float denominator;
+	float quotient;
+
+  cout << "Enter the numerator: ";
+  cin >> numerator;
+  cout << "Enter the denominator: ";
+  cin >> denominator;
+
+	quotient = divide(numerator, denominator);
+	cout << "The quotient is: " << quotient << endl;
+}
